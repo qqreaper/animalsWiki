a647fb1e1ada6ef6716b975911332311b4e92056
kdaniilm <kdaniilm@gmail.com>
2020-09-06 16:38:53 +0300

    Create classses and some logic
    Созданы классы Specifications, Anilas, DogView и логика отношений между ними, а также данные о собаках

---
+++ AnimalsWiki/AnimalsWiki/Specifications.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalsWiki
+{
+    class Specifications
+    {
+        public decimal Height;
+        public string Name;
+        public int LifeTime;
+        public decimal Weight;
+
+        public Specifications CreateSpecifications(decimal height, string name, int lifeTime, decimal weight)
+        {
+            Specifications specifications = new Specifications();
+            specifications.Height = height;
+            specifications.Name = name;
+            specifications.LifeTime = lifeTime;
+            specifications.Weight = weight;
+
+            return specifications;
+        }
+    }
+}
